e39d46b0c18ff2de7d6bdd9882e143840dfbec80
OBurnsy22 <mburns22@mail.csuchico.edu>
2017-10-02 17:37:13 -0700

    Commented some of the steps needed to complete the project

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//Author: Matthew Burns
+#include <iostream>
+#include <vector>
+#include "bitmap.h"
+#include <string>
+
+int main() {
+
+//ask user for picture, must be a bmp.
+//if user submits a non bmp file, alert them so they can submit the correct file
+//check to see if they submitted the correct file                       is image()
+//save a new version before any changes are made     save("newversion.bmp"}
+//send the photo to the pixel matrix so it can be edited             topixelmatrix()     
+//to make a pixel greyspace, take the values of the R, G, B colorscales and add them all togethor and divide by three.  That is the value that must be shared between each rgb of a pixel to make it greyspace
+//have a loop convert each pixel in the photo to greyspace going row by row in the matricies.
+//Check to make sure all pixels have been converted
+//Take photo from pixel matrix                   frompixelmatrix()
+//make sure the old file was not/does not get overwritten.
+
+
+
+
+
+return 0;
+}
+
