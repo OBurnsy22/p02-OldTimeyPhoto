802c6f65fb1ed4210fc19ac4777f063aa8f23a06
OBurnsy22 <mburns22@mail.csuchico.edu>
2017-10-11 17:02:15 -0700

    First version (not working yet)

--- main.cpp
+++ main.cpp
@@ -7,32 +7,50 @@
 using namespace std;
 
 int main() {
-    
+
     int Rows;
     int Columns;
 
-
-//ask user for picture, must be a bmp.
-//if user submits a non bmp file, alert them so they can submit the correct file
-//check to see if they submitted the correct file                       is image()
-//save a new version before any changes are made     save("newversion.bmp"}
-//send the photo to the pixel matrix so it can be edited             topixelmatrix()     
-//to make a pixel greyspace, take the values of the R, G, B colorscales and add them all togethor and divide by three.  That is the value that must be shared between each rgb of a pixel to make it greyspace
-//have a loop convert each pixel in the photo to greyspace going row by row in the matricies.
-//Check to make sure all pixels have been converted
-//Take photo from pixel matrix                   frompixelmatrix()
-//make sure the old file was not/does not get overwritten.
     Bitmap image;
     vector <vector <Pixel> > bmp;
     Pixel rgb;
-
-    image.open("machupicchu.bmp");
-    bmp = image.toPixelMatrix();
-    Rows = bmp.size();
-    Columns = bmp[0].size();
-    cout<<"machupicchu.bmp has been loaded. It is "<<Columns<<" pixels wide and "<<Rows<<" pixels high"<<endl;
-
     
+    do{
+   
+      cout<<"Hello, please enter the file name of an image (must be in Windows BMP format), if you are seeing theis message repeatedly, it means the file you are entering is not of the Windows BMP format."<<endl;
+      cin>>VARIABLE>>endl;
+     
+      bool validBmp = image.isImage(VARIABLE);
+    
+    
+
+      if(validBmp == true)
+      {
+      image.open("VARIABLE")
+      bmp = image.toPixelMatrix();
+      Rows = bmp.size();
+      Columns = bmp[0].size();
+      cout<<VARIABLE<<".bmp has been loaded. It is "<<Columns<<" pixels wide and "<<Rows<<" pixels high"<<endl;
+
+        for(int x=0; x<bmp.size(); x++)
+         {
+
+           for(int y=0; y<bmp[x].size(); y++)
+            {
+
+             rgb = bmp[x][y]
+             rgb = (rgb.red[x][y] + rgb.green[x][y] + rgb.blue[x][y])/ 3;
+             bmp[x][y] = rgb;
+          
+            }
+        }
+       
+      image.fromPixelMatrix(bmp);
+      image.save("oldtimey.bmp")
+
+      }
+
+      }while(vaildBmp == false)
 
 
 
